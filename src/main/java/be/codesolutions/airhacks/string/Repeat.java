a7163a488070e61e6bb78080799521a93ad57e8e
Frederick Gaens <frederick@codesolutions.be>
2025-07-29 08:44:38 +0200

    string repeat example

---
+++ src/main/java/be/codesolutions/airhacks/string/Repeat.java
@@ -0,0 +1,9 @@
+package be.codesolutions.airhacks.string;
+
+public class Repeat {
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.repeat(3));
+    }
+}
